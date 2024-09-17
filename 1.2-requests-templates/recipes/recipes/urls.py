df3487e2f55b68c1b2c4b800c7bb73dc71967e08
Nina Yurchenko <NMYurchenko@outlook.com>
2024-09-17 15:38:33 +0300

    Добавление измененных файлов и нового calculator/urls.py

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -1,21 +1,11 @@
-"""recipes URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path
+from django.contrib import admin  
+# Импортируем админку Django
+from django.urls import path, include  
+# Импортируем функции path и include
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('admin/', admin.site.urls),  
+    # Путь для админки
+    path('recipes/', include('calculator.urls')),  
+    # Подключаем URL-ы приложения calculator
 ]
